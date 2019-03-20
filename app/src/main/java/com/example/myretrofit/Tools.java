c6dc471f5b8e470968da3ff06ae8111bd374d359
jixiang <373105324@qq.com>
2019-03-20 11:05:35 +0800

    提交测试 将项目作为其他项目gradle依赖

---
+++ app/src/main/java/com/example/myretrofit/Tools.java
@@ -0,0 +1,13 @@
+package com.example.myretrofit;
+
+/**
+ * ClassName: Tools<br/>
+ * Description: TODO Description. <br/>
+ * date: 2019/3/20 11:04<br/>
+ *
+ * @author jixiang
+ * @version V1.0.0
+ * @since JDK 1.8
+ */
+public class Tools {
+}
---
+++ app/src/main/java/com/example/myretrofit/Utils.java
@@ -0,0 +1,17 @@
+package com.example.myretrofit;
+
+/**
+ * ClassName: Utils<br/>
+ * Description: TODO Description. <br/>
+ * date: 2019/3/20 10:59<br/>
+ *
+ * @author YCKJ1729
+ * @version V1.0.0
+ * @since JDK 1.8
+ */
+public class Utils {
+
+    public static void getTest(){
+
+    }
+}
