9785b7a81bf69e60148652660cc1465f1ff2a268
jixiang <373105324@qq.com>
2019-03-21 17:45:56 +0800

    去mylibrary1中lint功能 处理javadoc生成导致的编码不通过问题

---
+++ mylibrary1/src/main/java/com/example/mylibrary1/TestUtils.java
@@ -0,0 +1,26 @@
+package com.example.mylibrary1;
+
+/**
+ * ClassName: TestUtils<br/>
+ * Description: TODO Description. <br/>
+ * date: 2019/3/21 17:17<br/>
+ *
+ * @author jixiang
+ * @version V1.0.0
+ * @since JDK 1.8
+ */
+public class TestUtils {
+    private static  TestUtils instance;
+
+    /**
+     * 单例模式
+     */
+    public static TestUtils getInstance(){
+        if(instance==null){
+            instance=new TestUtils();
+        }
+        return instance;
+    }
+
+    private TestUtils(){}
+}
